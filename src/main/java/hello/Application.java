aee3363daff8784c1b76749a27d7939aa255786b
menya <>
2020-06-05 10:22:02 +0900

    no message

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -13,6 +13,10 @@ public class Application {
     public String home() {
         return "Hello Docker World";
     }
+    @RequestMapping("/alert-manager")
+    public String home() {
+        return "Hello Docker World";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
